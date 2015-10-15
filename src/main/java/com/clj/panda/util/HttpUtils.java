7780856debf040c0631e5d77f9deb89be2860290
smileclj <842705148@qq.com>
2015-10-15 18:49:58 +0800

    xx

--- src/main/java/com/clj/panda/util/HttpUtils.java
+++ src/main/java/com/clj/panda/util/HttpUtils.java
@@ -1,13 +1,36 @@
 package com.clj.panda.util;
 
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Created by lao on 2015/9/29.
  */
 public class HttpUtils {
     private static Logger logger = LoggerFactory.getLogger(HttpUtils.class);
+
+    public static String get(String url){
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet request = new HttpGet(url);
+        request.
+        CloseableHttpResponse response = null;
+        BufferedReader in = null;
+        try {
+            response = httpclient.execute(request);
+            in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            httpclient.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("".getBytes().length);
